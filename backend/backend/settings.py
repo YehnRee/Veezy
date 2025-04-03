9cd07fe577e434668db019ffe8c3968c4cf632ee
ONAIRE LAYUG <layugnaenae@gmail.com>
2025-04-04 00:33:06 +0800

    Added trusted origins frontend url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,11 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://your-frontend.vercel.app"
+    "https://veezy.vercel.app/"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://veezy.vercel.app/",
 ]
 
 ROOT_URLCONF = 'backend.urls'
